560d4c4c68272ebd696d9ffb24a4ae4397f9efa0
abui304 <atb2923@utexas.edu>
2025-09-22 01:55:46 -0500

    Hello

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,12 +5,12 @@ export default function NavBar() {
     return (
         <nav className="navbar">
             <h1 className="logo">PolitiKnow</h1>
-            <u1 className="nav-links">
+            <ul className="nav-links">
                 <li><a href="/profile">Profile</a></li>
                 <li><a href="/home">Home</a></li>
                 <li><a href="/search">Search</a></li>
                 <li><a href="/settings">Settings</a></li>
-            </u1>
+            </ul>
         </nav>
     );
 }
