e5afc58e7fa146f9ec7a7ccdb97e3008683147c4
nifemisoyemi <adenifemisoyemi@hotmail.com>
2025-09-26 15:49:40 -0500

    nifemi's first commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import "./NavBar.css";
 export default function NavBar() {
     return (
         <nav className="navbar">
-            <h1 className="logo">PolitiKnow</h1>
+            <a className="logo" href="/home"><img src="/PolitiKnow.png" alt="PolitiKnow"></img></a>
             <ul className="nav-links">
                 <li><a href="/profile">Profile</a></li>
                 <li><a href="/home">Home</a></li>
